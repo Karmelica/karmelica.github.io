be19eaaa2ebffedc27bb68b8fca272735ea8de85
Damian Niedziółka <damianniedziolka7@gmail.com>
2025-10-06 01:48:13 +0200

    Refactor navigation: Replace links with clickable elements and enhance styling for better usability

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const button = document.getElementById("showGame")
 button.addEventListener("click", showGame);
 
+const infoLink = document.getElementById("info-click");
+infoLink.addEventListener("click", () => scrollToSection("about"));
+
+const projectsLink = document.getElementById("projects-click");
+projectsLink.addEventListener("click", () => scrollToSection("projects"));
+
 function showGame(){
     const iframe1 = document.getElementById("elektron");
     if(iframe1.style.display === "unset"){
@@ -10,4 +16,9 @@ function showGame(){
     }
     iframe1.style.display = "unset";
     button.innerText = "Ukryj grę";
+}
+
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: "smooth" });
 }
