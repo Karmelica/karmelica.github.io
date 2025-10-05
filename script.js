e2d80d4247403bfbd4adc1f3402ea0f6b3816f8d
Damian Niedziółka <damianniedziolka7@gmail.com>
2025-10-06 01:18:19 +0200

    Enhance UI: Add navigation links and style button for better user experience

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const button = document.getElementById("showGame")
-button.addEventListener("click", show);
+button.addEventListener("click", showGame);
 
-function show(){
+function showGame(){
     const iframe1 = document.getElementById("elektron");
     if(iframe1.style.display === "unset"){
         iframe1.style.display = "none";
+        button.innerText = "Zagraj teraz";
         return;
     }
     iframe1.style.display = "unset";
+    button.innerText = "Ukryj grę";
 }
